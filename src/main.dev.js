25ad1bf195a6e2276d69f61850b0c2ff50ef8afd
jeffvli <jeffvictorli@gmail.com>
2022-01-08 15:44:07 -0800

    test

--- src/main.dev.js
+++ src/main.dev.js
@@ -20,14 +20,14 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import { configureStore } from '@reduxjs/toolkit';
 import { forwardToRenderer, triggerAlias, replayActionMain } from 'electron-redux';
-import {
-  MediaPlaybackStatus,
-  MediaPlaybackType,
-  SystemMediaTransportControlsButton,
-} from '@nodert-win10-au/windows.media';
-import { BackgroundMediaPlayer } from '@nodert-win10-au/windows.media.playback';
-import { RandomAccessStreamReference } from '@nodert-win10-au/windows.storage.streams';
-import { Uri } from '@nodert-win10-au/windows.foundation';
+// import {
+//   MediaPlaybackStatus,
+//   MediaPlaybackType,
+//   SystemMediaTransportControlsButton,
+// } from '@nodert-win10-au/windows.media';
+// import { BackgroundMediaPlayer } from '@nodert-win10-au/windows.media.playback';
+// import { RandomAccessStreamReference } from '@nodert-win10-au/windows.storage.streams';
+// import { Uri } from '@nodert-win10-au/windows.foundation';
 import playerReducer, { setStatus } from './redux/playerSlice';
 import playQueueReducer, {
   decrementCurrentIndex,
@@ -345,7 +345,12 @@ if (isLinux) {
 }
 
 if (isWindows && isWindows10) {
-  const Controls = BackgroundMediaPlayer.current.systemMediaTransportControls;
+  const windowsMedia = require('@nodert-win10-au/windows.media');
+  const windowsMediaPlayback = require('@nodert-win10-au/windows.media.playback');
+  const windowsStorageStreams = require('@nodert-win10-au/windows.storage.streams');
+  const windowsFoundation = require('@nodert-win10-au/windows.foundation');
+
+  const Controls = windowsMediaPlayback.BackgroundMediaPlayer.current.systemMediaTransportControls;
 
   if (settings.getSync('systemMediaTransportControls')) {
     Controls.isEnabled = true;
@@ -372,8 +377,8 @@ if (isWindows && isWindows10) {
   Controls.isNextEnabled = true;
   Controls.isPreviousEnabled = true;
 
-  Controls.playbackStatus = MediaPlaybackStatus.closed;
-  Controls.displayUpdater.type = MediaPlaybackType.music;
+  Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.closed;
+  Controls.displayUpdater.type = windowsMedia.MediaPlaybackType.music;
 
   Controls.displayUpdater.musicProperties.title = 'Sonixd';
   Controls.displayUpdater.musicProperties.artist = 'No Track Playing';
@@ -382,22 +387,22 @@ if (isWindows && isWindows10) {
 
   Controls.on('buttonpressed', (sender, eventArgs) => {
     switch (eventArgs.button) {
-      case SystemMediaTransportControlsButton.play:
+      case windowsMedia.SystemMediaTransportControlsButton.play:
         play();
-        Controls.playbackStatus = MediaPlaybackStatus.playing;
+        Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.playing;
         break;
-      case SystemMediaTransportControlsButton.pause:
+      case windowsMedia.SystemMediaTransportControlsButton.pause:
         pause();
-        Controls.playbackStatus = MediaPlaybackStatus.paused;
+        Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.paused;
         break;
-      case SystemMediaTransportControlsButton.stop:
+      case windowsMedia.SystemMediaTransportControlsButton.stop:
         stop();
-        Controls.playbackStatus = MediaPlaybackStatus.stopped;
+        Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.stopped;
         break;
-      case SystemMediaTransportControlsButton.next:
+      case windowsMedia.SystemMediaTransportControlsButton.next:
         nextTrack();
         break;
-      case SystemMediaTransportControlsButton.previous:
+      case windowsMedia.SystemMediaTransportControlsButton.previous:
         previousTrack();
         break;
       default:
@@ -407,15 +412,15 @@ if (isWindows && isWindows10) {
 
   ipcMain.on('playpause', (_event, arg) => {
     if (arg.status === 'PLAYING') {
-      Controls.playbackStatus = MediaPlaybackStatus.playing;
+      Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.playing;
     } else {
-      Controls.playbackStatus = MediaPlaybackStatus.paused;
+      Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.paused;
     }
   });
 
   ipcMain.on('current-song', (_event, arg) => {
-    if (Controls.playbackStatus !== MediaPlaybackStatus.playing) {
-      Controls.playbackStatus = MediaPlaybackStatus.playing;
+    if (Controls.playbackStatus !== windowsMedia.MediaPlaybackStatus.playing) {
+      Controls.playbackStatus = windowsMedia.MediaPlaybackStatus.playing;
     }
 
     Controls.displayUpdater.musicProperties.title = arg.title || 'Unknown Title';
@@ -425,8 +430,8 @@ if (isWindows && isWindows10) {
         : 'Unknown Artist';
     Controls.displayUpdater.musicProperties.albumTitle = arg.album || 'Unknown Album';
 
-    Controls.displayUpdater.thumbnail = RandomAccessStreamReference.createFromUri(
-      new Uri(
+    Controls.displayUpdater.thumbnail = windowsStorageStreams.RandomAccessStreamReference.createFromUri(
+      new windowsFoundation.Uri(
         arg.image.includes('placeholder')
           ? 'https://raw.githubusercontent.com/jeffvli/sonixd/main/src/img/placeholder.png'
           : arg.image
